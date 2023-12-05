3bfad7fe8adf57a355be11212f3c76f179e3623b
r1d3rzz <myintthwaykhine127@gmail.com>
2023-12-05 12:05:16 +0630

    fetch user data in employees datatable route using rs with user->department table and fix bug in emController ssd return view path

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -16,16 +16,26 @@ class EmployeeController extends Controller
     public function ssd()
     {
         if (\request()->ajax()) {
-            $data = User::latest()->get();
+            $data = User::with("department")->latest()->get();
             return DataTables::of($data)
                 ->addIndexColumn()
                 ->addColumn('action', function ($row) {
                     $actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
                     return $actionBtn;
                 })
-                ->rawColumns(['action'])
+                ->addColumn('department_name', function ($each) {
+                    return $each->department ? $each->department->title : "-";
+                })
+                ->editColumn('is_present', function ($each) {
+                    if ($each->is_present) {
+                        return "<span class='badge bg-primary'>Present</span>";
+                    } else {
+                        return "<span class='badge bg-danger'>Leave</span>";
+                    }
+                })
+                ->rawColumns(['action', 'is_present'])
                 ->make(true);
         }
-        return view('products.index');
+        return view('employee.index');
     }
 }
--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -3,15 +3,22 @@
 
 @section("content")
 
-<table class="table table-bordered" id="employees">
-    <thead>
-        <tr class="text-center">
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-        </tr>
-    </thead>
-</table>
+<div class="card mt-2">
+    <div class="card-body">
+        <table class="table table-bordered" id="employees">
+            <thead>
+                <tr class="text-center">
+                    <th>Employee ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Department</th>
+                    <th>Is Present?</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
 
 @endsection
 
@@ -23,9 +30,12 @@
             serverSide: true,
             ajax: "{{ route('employees.dbtable') }}",
             columns: [
+                {data: 'employee_id', name: 'employee_id'},
                 {data: 'name', name: 'name'},
                 {data: 'email', name: 'email'},
                 {data: 'phone', name: 'phone'},
+                {data: 'department_name', name: 'department_name'},
+                {data: 'is_present', name: 'is_present',class:"text-center"},
             ]
         });
     });
